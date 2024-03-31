d79fd4e71438cdf973ed47665d64e404f6dd6938
jamesbrowncyril <jamesbrowncyril@gmail.com>
2024-03-31 19:19:15 +0300

    added all these

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
+const cors = require("cors")
+app.use(cors())
 app.listen(5000,()=>{
-console.log("app running on port 5000")
+console.log("app running on port 500000")
 })
 app.get('/',(req,res)=>{
-    res.json({message:"success"})
+    res.json({message:"successyyyutyytttv"})
 })
